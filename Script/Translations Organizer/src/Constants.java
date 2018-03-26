58ba5abb97bb6e136029fc1b9dcff62b94d772eb
TimoBergerbusch <timo-bergerbusch@web.de>
2018-03-26 18:11:08 +0200

    Added dynamic Texture-Requirements

--- Script/Translations Organizer/src/Constants.java
+++ Script/Translations Organizer/src/Constants.java
@@ -1,5 +1,10 @@
+import org.ini4j.Ini;
+
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Set;
 
 /**
  * Created by Timo Bergerbusch on 12.02.2018.
@@ -14,15 +19,38 @@ class Constants {
     public static final Color neutral = Color.lightGray;
     public static final String defaultPath = System.getenv("APPDATA") + "\\SketchUp\\SketchUp 2018\\SketchUp\\Plugins";
 
-    public static final Requirement requirement = initRequirement();
-
     public static final String excelConstantSectionName = "RobersExcelConstants";
     public static final String excelConstantsPath = defaultPath + "\\su_RobersExcelConvert\\classes\\constants.ini";
     public static final String[] excelConstants = new String[]{"headerRow", "Lfd", "SageArt", "Bezeichnung", "Bauteil", "Materialgruppe", "Werkstoff", "Anzahl", "Laenge", "Breite", "Hoehe"};
 
+    public static final String materialsPath = defaultPath + "\\su_RobersExcelConvert\\classes\\materials.ini";
+
     public static Material errorMaterial;
 
+    public static final Requirement requirement = initRequirement();
+
     private static Requirement initRequirement() {
+
+        ArrayList<String> names = new ArrayList<>();
+
+        try {
+            Ini ini = new Ini(new File(materialsPath));
+            Set<String> keys = ini.keySet();
+            String[] sideNames = new String[]{"vorne", "hinten", "links", "rechts", "oben", "unten"};
+            String name;
+            for (String section : keys) {
+                for (String side : sideNames) {
+                    name = ini.get(section, side);
+                    if (!names.contains(name)) {
+                        names.add(name);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        ArrayList<Requirement> texturen = getMaterialsAsRequirement(names);
 
         return new Requirement("SketchUp", "", new ArrayList<Requirement>() {{
             // Pfad: SketchUp/Plugins
@@ -46,18 +74,36 @@ class Constants {
                         add(new Requirement("Translations", "translations.ini", null));
                     }}));
                     // Pfad: SketchUp/Plugins/su_RobersExcelConvert/textures
-                    add(new Requirement("Texturen", "textures", new ArrayList<Requirement>() {{
-                        add(new Requirement("errorTexture", "errorTexture.jpg", null));
-                        add(new Requirement("texture1", "texture1.jpg", null));
-                        add(new Requirement("texture2", "texture2.jpg", null));
-                        add(new Requirement("texture3", "texture3.jpg", null));
-                        add(new Requirement("texture4", "texture4.jpg", null));
-                        add(new Requirement("texture5", "texture5.jpg", null));
-                        add(new Requirement("texture6", "texture6.jpg", null));
-                    }}));
+                    add(new Requirement("Texturen", "textures", //new ArrayList<Requirement>() {{
+                            texturen
+//                        add(new Requirement("errorTexture", "errorTexture.jpg", null));
+//                        add(new Requirement("texture1", "texture1.jpg", null));
+//                        add(new Requirement("texture2", "texture2.jpg", null));
+//                        add(new Requirement("texture3", "texture3.jpg", null));
+//                        add(new Requirement("texture4", "texture4.jpg", null));
+//                        add(new Requirement("texture5", "texture5.jpg", null));
+//                        add(new Requirement("texture6", "texture6.jpg", null));
+//                    }}));
+                    ));
                 }}));
             }}
             ));
         }});
+    }
+
+    private static ArrayList<Requirement> getMaterialsAsRequirement(ArrayList<String> names) {
+        ArrayList<Requirement> texturen = new ArrayList<>();
+
+        texturen.add(new Requirement("errorTexture", "errorTexture.jpg", null));
+        for (String name : names) {
+            texturen.add(getMaterialAsRequirement(name));
+        }
+
+        return texturen;
+    }
+
+    private static Requirement getMaterialAsRequirement(String name) {
+
+        return new Requirement(name, name + ".jpg", null);
     }
 }
